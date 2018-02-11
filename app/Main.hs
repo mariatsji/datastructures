1bb335c4dd9c27674ddc3d58bd1a2b9ed1cbb5a4
Sjur Millidahl <sjur.millidahl@finn.no>
2018-02-11 12:24:02 +0100

    use basic forkIO and IORef

--- app/Main.hs
+++ app/Main.hs
@@ -2,35 +2,13 @@ module Main where
 
 import Control.Concurrent
 import Control.Monad
-import Control.Concurrent.STM
-
-funThreads :: IO ()
-funThreads = do
-   threadId <- forkIO (do
-     tId <- myThreadId
-     forever (do
-       putStrLn $ "This is in first thread " ++ show tId))
-   tId <- myThreadId
-   forever (putStrLn $ "This is in second thread " ++ show tId)
-
-
-inc :: Enum a => STM (TVar a) -> IO ()
-inc s = do
-  iRef <- atomically s
-  let m = modifyTVar' iRef succ
-  print "subthread succ"
-
-read' :: Show a => STM (TVar a) -> IO ()
-read' s = do
-  tvar <- atomically s
-  val <- readTVarIO tvar
-  print val
+import Data.IORef
 
 main :: IO ()
 main = do
-  let stm = newTVar 0 :: STM (TVar Int)
-  forkIO ( inc stm )
-  forkIO ( read' stm )
-  threadDelay 1000
-
+  ref <- newIORef 0 :: IO (IORef Int)
+  forkIO ( modifyIORef' ref succ )
+  threadDelay 100
+  val <- readIORef ref
+  print val
 
