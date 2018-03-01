51a3b73e28fdd7cc9031fcf0e7a070ad9b0e5b45
Sjur Millidahl <sjur.millidahl@finn.no>
2018-03-01 08:29:44 +0100

    works

---
+++ app/AppStack.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module AppStack where
+
+import           Control.Monad.Reader
+import           Control.Monad.State
+import           Control.Monad.Writer
+import           Data.Monoid
+import           System.Directory
+import           System.FilePath
+
+newtype AppConfig = AppConfig { cfgMaxDepth :: Int
+} deriving (Show)
+
+newtype AppState = AppState { stDeepestReached :: Int
+} deriving (Show)
+
+newtype AppLog = AppLog { aLog :: String
+} deriving (Show)
+
+instance Monoid AppLog where
+    mempty = AppLog ""
+    mappend a b = AppLog (aLog a ++ aLog b)
+
+newtype MyApp a = MyA {
+    runA :: WriterT AppLog (StateT AppState (ReaderT AppConfig IO)) a
+} deriving (Functor, Applicative, Monad, MonadIO, MonadWriter AppLog, MonadReader AppConfig,
+  MonadState AppState)
+
+runApp :: MyApp a -> Int -> IO ((a, AppLog), AppState)
+runApp k maxDepth =
+    let config = AppConfig maxDepth
+        state = AppState 0
+        myLog = AppLog ""
+    in runReaderT (runStateT (runWriterT (runA k)) state) config
+
+constrainedCount :: Int -> FilePath -> MyApp [(FilePath, Int)]
+constrainedCount curDepth path = do
+    contents <- liftIO . listDirectory $ path
+    cfg <- ask
+    rest <- forM contents $ \name -> do
+            let newPath = path </> name
+            isDir <- liftIO $ doesDirectoryExist newPath
+            if isDir && curDepth < cfgMaxDepth cfg
+                then do
+                    let newDepth = curDepth + 1
+                    st <- get
+                    when (stDeepestReached st < newDepth) $
+                        put st { stDeepestReached = newDepth }
+                    constrainedCount newDepth newPath
+                else return []
+    return $ (path, length contents) : concat rest
--- app/Main.hs
+++ app/Main.hs
@@ -3,42 +3,23 @@
 module Main where
 
 import           Control.Monad.Reader
-import           Control.Monad.State
-import           System.Directory
-import           System.FilePath
+import           Control.Monad.Writer
 
-newtype AppConfig = AppConfig { cfgMaxDepth :: Int
-} deriving (Show)
+data AppConfig = AppConfig deriving (Show)
 
-newtype AppState = AppState { stDeepestReached :: Int
-} deriving (Show)
+type App a = WriterT [(Int, String)] (ReaderT AppConfig IO) a
 
-newtype MyApp a = MyA {
-    runA :: StateT AppState (ReaderT AppConfig IO) a
-} deriving (Functor, Applicative, Monad, MonadIO, MonadReader AppConfig,
-  MonadState AppState)
+zeList = [5,26,4]
 
-runApp :: MyApp a -> Int -> IO (a, AppState)
-runApp k maxDepth =
-    let config = AppConfig maxDepth
-        state = AppState 0
-    in runReaderT (runStateT (runA k) state) config
+visit :: Int -> [(Int, String)]
+visit i = [(i, "Visited [" ++ show i ++ "]")]
 
-constrainedCount :: Int -> FilePath -> MyApp [(FilePath, Int)]
-constrainedCount curDepth path = do
-    contents <- liftIO . listDirectory $ path
-    cfg <- ask
-    rest <- forM contents $ \name -> do
-            let newPath = path </> name
-            isDir <- liftIO $ doesDirectoryExist newPath
-            if isDir && curDepth < cfgMaxDepth cfg
-                then do
-                    let newDepth = curDepth + 1
-                    st <- get
-                    when (stDeepestReached st < newDepth) $
-                        put st { stDeepestReached = newDepth }
-                    constrainedCount newDepth newPath
-                else return []
-    return $ (path, length contents) : concat rest
+processList :: [Int] -> App [()]
+processList = mapM (tell . visit)
 
-main = return ()
+main :: IO ()
+main = do
+    let res = runWriterT $ processList zeList
+    let oo = runReaderT res AppConfig
+    hmm <- liftIO oo
+    print hmm
